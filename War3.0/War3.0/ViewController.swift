a2b3f6004d38f28c694dde20b0e4a7f33ed978cb
Shanizzle <shannenreese@yahoo.com>
2016-11-14 22:20:54 -0600

    does is it show now?

--- War3.0/War3.0/ViewController.swift
+++ War3.0/War3.0/ViewController.swift
@@ -16,6 +16,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         // Making a change!!!
+        // What the :|
     }
 
     override func didReceiveMemoryWarning() {
