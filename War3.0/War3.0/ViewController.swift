a554738d2bd329605de38ef6df9ed567368df2f3
Shanizzle <shannenreese@yahoo.com>
2016-11-15 22:04:40 -0600

    Finished War3

--- War3.0/War3.0/ViewController.swift
+++ War3.0/War3.0/ViewController.swift
@@ -10,7 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var RightImageView: UIImageView!
+    
+    @IBOutlet weak var rightImageView: UIImageView!
+    @IBOutlet weak var rightScoreLabel: UILabel!
+    var rightScore = 0
+    
+    
+    @IBOutlet weak var leftImageView: UIImageView!
+    @IBOutlet weak var leftScoreLabel: UILabel!
+    var leftScore = 0
+    
+    
+    
+    var cardNames = ["card2", "card3", "card4", "card5", "card6", "card7", "card8", "card9", "card10", "jack", "queen", "king", "ace"]
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,6 +37,42 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
 
+    @IBAction func dealTapped(_ sender: UIButton) {
+        
+        
+        let leftNumber = Int(arc4random_uniform(13));
+        let rightNumber = Int(arc4random_uniform(13));
+        
+        rightImageView.image = UIImage(named: cardNames[rightNumber])
+        leftImageView.image = UIImage(named: cardNames[leftNumber])
+        
+        
+        if leftNumber > rightNumber {
+            
+            leftScore += 1
+            
+            leftScoreLabel.text = String(leftScore)
+            
+            
+        }
+        else if leftNumber == rightNumber {
+            
+        }
+        else {
+            
+            rightScore += 1
+            rightScoreLabel.text = String(rightScore)
+        }
+        
+        
+    }
+    
+    
+    
+    
+    
+    
 }
 
